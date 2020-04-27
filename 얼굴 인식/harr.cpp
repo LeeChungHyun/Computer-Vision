9b6ac5e6e1c5bb96f2ca0121374976f35969900c
LeeChungHyun <38039844+LeeChungHyun@users.noreply.github.com>
2020-04-28 08:45:41 +0900

    Update harr.cpp

--- 얼굴 인식/harr.cpp
+++ 얼굴 인식/harr.cpp
@@ -18,12 +18,14 @@ int main()
 }
 
 /*얼굴 검출 함수입니다.
+
 1.이미지를 imread시킨다==>visual studio로 할 때, 이미지와 xml파일들은 해당 프로젝트 폴더에 옮기셔야 합니다.
 
-2. xml을 불러들인다==>해당xml 파일들은 haarcascade의 전면 얼굴(frontalface_default)과 눈(eye)에 대한 내용이 들어있습니다. 이는 opencv에서 제공해주므로 따로 만들 필요가 없습니다.
-해당 xml 파일은 c:/opencv/source/data/haarcascades에 위치해 있습니다.
+2. xml을 불러들인다==>해당xml 파일들은 haarcascade의 전면 얼굴(frontalface_default)과 눈(eye)에 대한 내용이 들어있습니다.
+이는 opencv에서 제공해주므로 따로 만들 필요가 없습니다. 해당 xml 파일은 c:/opencv/source/data/haarcascades에 위치해 있습니다.
 
-3. 입력 영상 image에서 다양한 크기의 객체 사각형 영역을 검출합니다(detectMultiScale). Rect클래스를 이용해 벡터 타입으로 모든 사각형 정보를 저장합니다.
+3. 입력 영상 image에서 다양한 크기의 객체 사각형 영역을 검출합니다(detectMultiScale).
+Rect클래스를 이용해 벡터 타입으로 모든 사각형 정보를 저장합니다.
 */
 void detect_face()
 {
@@ -56,10 +58,15 @@ void detect_face()
 	destroyAllWindows();
 }
 /*눈 영역을 검출합니다. 전체적인 코드는 위와 비슷합니다.
+
 1. 먼저 얼굴의 위치를 찾은 후에 눈 위치를 찾는 방식으로 진행했습니다.
+
 2. 입력 영상에서 검출한 사각형 얼굴 영역의 부분 영상을 추출하여 faceROI에 저장합니다.
+
 3. faceROI에서 눈을 검출합니다.
-4. 검출한 눈의 중앙에 원을 그립니다.*/
+
+4. 검출한 눈의 중앙에 원을 그립니다.
+*/
 void detect_eyes()
 {
 	Mat src = imread("lena.jpg");
