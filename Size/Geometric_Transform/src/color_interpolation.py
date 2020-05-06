749f73ab013205e0afdf93cb26daeb742eadb15d
LeeChungHyun <38039844+LeeChungHyun@users.noreply.github.com>
2020-05-07 00:22:13 +0900

    Update color_interpolation.py

--- Size/Geometric_Transform/src/color_interpolation.py
+++ Size/Geometric_Transform/src/color_interpolation.py
@@ -1,26 +1,19 @@
 import cv2 as cv
 import numpy as np
 
-
 def color_rect():
-
-    # Color rectangle 생성
-	#opencv 파이썬에서는 특이하게 RGB가 아닌 BGR로 배치된다.
-
-	g = np.zeros((256, 256, 3), np.uint8)
-	
-	#r = np.uint8([[[0,0,255]]])
+ 
+	g = np.zeros((256,256,3), np.uint8)
+ 
 	for i in range(256):
-    		for j in range(256):
-    				
-		g[i, :, :] = (0, i, 255-i)
-		g[i, :, :] = (i, 0, 255-i)
+    		g[i,:,:] = (0, i, 255-i)
+        g[i, 255, :] = (255-i,i,i)
+        for j in range(256):
+              g[i,j,:] = (1-j/256)*src[i,0,:] + j/256*g[i,255,:]
+
 	return g
 
 g = color_rect()
-#dst = cv.add(g, 100)
-
 cv.imshow('Org', g)
-
 cv.waitKey(0)
 cv.destroyAllWindows()
